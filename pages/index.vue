a16330fb537630278cdd3195522529ea89a5cb1c
jdanks <jdanks.army@outlook.com>
2021-02-03 13:11:29 +0100

    Removed skeleton header, added 'ago' date display

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,37 @@
 <template>
   <v-container fluid tag="div">
-    <link href='https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Playfair+Display&display=swap' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Playfair+Display&display=swap'
+          rel='stylesheet' type='text/css'>
     <link rel="prefetch" href="https://api.jdanks.army/streams">
 
     <v-row justify="center">
       <v-col class="mt-n5 content" tag="v-card">
-
-        <v-row tag="v-container" v-if="loading" class="mt-4 mb-2 d-flex align-center" style="width: 100%;">
-          <v-divider class="ml-3" />
-          <span class="mx-5 mt-n1 text-overline grey--text"> online streams </span>
-          <v-divider class="mr-3" />
-        </v-row>
         <v-skeleton-loader
             type="list-item-avatar-two-line"
             :loading="loading"
             :elevation="0"
             dark
-            :class="{'mt-n4': loading}"
+            :class="{'mt-n1': loading}"
             transition="v-fade-transition"
         >
 
           <v-container tag="div">
             <featured-streamer v-if="featuredStreamer" :streamer="featuredStreamer"/>
-            <stream-list-group :streamers="online" divider-title="Online streams" header-styling="grey--text" class="mt-n5" />
-            <stream-list-group :streamers="offline" divider-title="Offline streams" header-styling="accent--text" />
+            <stream-list-group :streamers="online" divider-title="Online streams" header-styling="grey--text"
+                               class="mt-n5"/>
+            <stream-list-group :streamers="offline" divider-title="Offline streams" header-styling="accent--text"/>
           </v-container>
 
         </v-skeleton-loader>
 
-        <v-skeleton-loader v-for="i in 22" :key="i" type="list-item-avatar-two-line" v-if="loading" :elevation="0" />
+        <v-skeleton-loader v-for="i in 22" :key="i" type="list-item-avatar-two-line" v-if="loading" :elevation="0"/>
 
       </v-col>
     </v-row>
 
     <v-card tag="div" class="pa-2 lastUpdated" @click="update" outlined>
       <v-skeleton-loader :loading="loading" type="text" width="200px">
-        Last updated at {{lastUpdated}}
+        Last updated {{ lastUpdated }}
       </v-skeleton-loader>
     </v-card>
 
@@ -57,7 +53,7 @@ export default {
   data() {
     return {
       streamer_data: [],
-      lastUpdated: "",
+      lastUpdatedDate: "",
 
       featuredStreamer: null,
 
@@ -75,7 +71,82 @@ export default {
     this.updateInterval = setInterval(this.update, 5 * 60 * 1000);
   },
 
+  computed: {
+
+    lastUpdated() {
+      return this.timeAgo(this.lastUpdatedDate);
+    }
+  },
+
   methods: {
+    getFormattedDate(date, prefomattedDate = false, hideYear = false) {
+      const MONTH_NAMES = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+      ];
+
+      const day = date.getDate();
+      const month = MONTH_NAMES[date.getMonth()];
+      const year = date.getFullYear();
+      const hours = date.getHours();
+      let minutes = date.getMinutes();
+
+      if (minutes < 10) {
+        // Adding leading zero to minutes
+        minutes = `0${minutes}`;
+      }
+
+      if (prefomattedDate) {
+        // Today at 10:20
+        // Yesterday at 10:20
+        return `${prefomattedDate} at ${hours}:${minutes}`;
+      }
+
+      if (hideYear) {
+        // 10. January at 10:20
+        return `${day}. ${month} at ${hours}:${minutes}`;
+      }
+
+      // 10. January 2017. at 10:20
+      return `${day}. ${month} ${year}. at ${hours}:${minutes}`;
+    },
+
+
+    timeAgo(dateParam) {
+      if (!dateParam) {
+        return null;
+      }
+
+      const date = typeof dateParam === 'object' ? dateParam : new Date(dateParam);
+      const DAY_IN_MS = 86400000; // 24 * 60 * 60 * 1000
+      const today = new Date();
+      const yesterday = new Date(today - DAY_IN_MS);
+      const seconds = Math.round((today - date) / 1000);
+      const minutes = Math.round(seconds / 60);
+      const isToday = today.toDateString() === date.toDateString();
+      const isYesterday = yesterday.toDateString() === date.toDateString();
+      const isThisYear = today.getFullYear() === date.getFullYear();
+
+
+      if (seconds < 5) {
+        return 'just now';
+      } else if (seconds < 60) {
+        return `${seconds} seconds ago`;
+      } else if (seconds < 90) {
+        return 'about a minute ago';
+      } else if (minutes < 60) {
+        return `${minutes} minutes ago`;
+      } else if (isToday) {
+        return this.getFormattedDate(date, 'Today'); // Today at 10:20
+      } else if (isYesterday) {
+        return this.getFormattedDate(date, 'Yesterday'); // Yesterday at 10:20
+      } else if (isThisYear) {
+        return this.getFormattedDate(date, false, true); // 10. January at 10:20
+      }
+
+      return this.getFormattedDate(date); // 10. January 2017. at 10:20
+    },
+
     filterStreamers() {
       this.offline = [];
       this.online = [];
@@ -93,7 +164,7 @@ export default {
       try {
         this.streamer_data = await this.$axios.$get('https://api.jdanks.army/streams');
         this.filterStreamers();
-        this.lastUpdated = new Date().toLocaleTimeString()
+        this.lastUpdatedDate = new Date();
       } catch (e) {
         console.error(e);
       }
@@ -108,14 +179,14 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  .content {
-    max-width: 600px;
-  }
+.content {
+  max-width: 600px;
+}
 
-  .lastUpdated {
-    position: fixed;
-    z-index: 10;
-    bottom: 0;
-    right: 0;
-  }
+.lastUpdated {
+  position: fixed;
+  z-index: 10;
+  bottom: 0;
+  right: 0;
+}
 </style>
